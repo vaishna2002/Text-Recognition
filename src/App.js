efc8cbbad953742e64429aa76aaea564b4487598
vaishna2002 <co.2020.vabhende@bitwardha.ac.in>
2023-11-20 16:14:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./App.css"
+import { useReactMediaRecorder } from 'react-media-recorder';
+import convertBlobToWav from 'audio-recorder-polyfill';
 
-function App() {
+const App = () => {
+  const { status, startRecording, stopRecording, mediaBlobUrl } = useReactMediaRecorder({
+    audio: true,
+  });
+
+  const handleDownload = async () => {
+    if (mediaBlobUrl) {
+      try {
+        const audioBlob = await fetch(mediaBlobUrl).then(response => response.blob());
+        localStorage.setItem('audio' , audioBlob)
+
+        // ... rest of your download logic
+      } catch (error) {
+        console.error('Error handling download:', error);
+      }
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container1">
+      {/* <p>{status}</p> */}
+      <div id="twobuttons">
+      <button onClick={startRecording}>Start Recording</button>
+      <button onClick={stopRecording}>Stop Recording</button>
+      </div>
+      <div id="record">
+      <audio src={mediaBlobUrl} controls autoPlay />
+      {mediaBlobUrl && (
+        <button1 onClick={handleDownload}>Download Recording</button1>
+      )}
+      </div>
+      <div id="showbutton">
+        <button onClick={()=>console.log(localStorage.getItem('audio'))} >Show</button>
+      </div>  
     </div>
   );
-}
+};
 
 export default App;
